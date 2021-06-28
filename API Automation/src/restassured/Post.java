6391581ce04fea2e65ce382fdb47b973e4491180
rahulmandve <rahul.mandwe259@gmail.com>
2021-06-28 11:58:51 +0530

    add body assertion

--- API Automation/src/restassured/Post.java
+++ API Automation/src/restassured/Post.java
@@ -2,12 +2,14 @@ package restassured;
 
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 public class Post {
 		
 	public static void main(String args[]) {
 		RestAssured.baseURI="http://dummy.restapiexample.com";
 		given().log().all().header("Content-Type", "application/json").body("{\"name\":\"test\",\"salary\":\"123\",\"age\":\"23\"}")
-		.when().post("api/v1/create").then().log().all().assertThat().statusCode(200);
+		.when().post("api/v1/create").then().log().all().assertThat().statusCode(200).body("message",equalTo("Successfully! Record has been added."));
+		//.header("Content-Type", "application/json");
 		
 	
 	}
